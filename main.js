0bde1aa36a2567eb2766dd38fe38c030dfa8f09c
mariagrasirosa <mariagrasielidarosa@gmail.com>
2022-05-28 18:42:00 -0300

    portfolio start

---
+++ main.js
@@ -0,0 +1,7 @@
+// show scroll top
+
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top');
+    if(this.scrollY >= 200) scrollTop.classList.add('show-scroll');else scrollTop.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollTop)
